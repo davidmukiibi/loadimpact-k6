db119a692086988c323790711a6ea6609a8e52e5
David Mukiibi <david.mukiibiq@gmail.com>
2019-12-02 21:24:09 +0300

    separated these 2

--- permit.sh
+++ permit.sh
@@ -1,29 +1,7 @@
 #!/bin/bash
 
 set -ex
-# set -o pipefail
 
 public_ip_address=$(curl -q http://checkip.amazonaws.com)
 
-authorize() {
-    /root/bin/aws ec2 authorize-security-group-ingress --group-id sg-811dfec4 --ip-permissions IpProtocol=tcp,FromPort=443,ToPort=443,IpRanges='[{CidrIp=${public_ip_address}/32}]'
-}
-
-revoke() {
-    /root/bin/aws ec2 revoke-security-group-ingress --group-id sg-811dfec4 --ip-permissions IpProtocol=tcp,FromPort=443,ToPort=443,IpRanges='[{CidrIp=${public_ip_address}/32}]'
-}
-
-main() {
-    if [ $1 == 'authorize' ]
-    then
-        authorize
-    elif [ $1 == 'revoke' ]
-    then
-        revoke
-    else
-        echo 'Do you want to revoke or authorize?'
-    fi
-
-}
-
-main "$@"
+/root/bin/aws ec2 authorize-security-group-ingress --group-id sg-811dfec4 --ip-permissions IpProtocol=tcp,FromPort=443,ToPort=443,IpRanges='[{CidrIp=${public_ip_address}/32}]'
---
+++ revoke.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -ex
+
+public_ip_address=$(curl -q http://checkip.amazonaws.com)
+
+/root/bin/aws ec2 revoke-security-group-ingress --group-id sg-811dfec4 --ip-permissions IpProtocol=tcp,FromPort=443,ToPort=443,IpRanges='[{CidrIp=${public_ip_address}/32}]'
+
