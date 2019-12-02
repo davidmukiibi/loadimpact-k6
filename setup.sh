af957c02fe5402047da944fefb787349316b8ae1
David Mukiibi <david.mukiibiq@gmail.com>
2019-12-02 20:48:54 +0300

    added k6 installation

--- setup.sh
+++ setup.sh
@@ -7,8 +7,15 @@ update_cache() {
     apt-get update
 }
 
+install_k6() {
+    sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 379CE192D401AB61
+    echo "deb https://dl.bintray.com/loadimpact/deb stable main" | sudo tee -a /etc/apt/sources.list
+    sudo apt-get update
+    sudo apt-get install k6
+}
+
 install_aws_cli() {
-    apt-get install curl unzip python2.7 python-pip -y
+    apt-get install curl unzip python2.7 python-pip sudo -y
     curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
     unzip awscli-bundle.zip
     ./awscli-bundle/install -b ~/bin/aws
@@ -26,6 +33,8 @@ main() {
     update_cache
     install_aws_cli
     configuring_aws
+    update_cache
+    install_k6
 }
 
 main "$@"
