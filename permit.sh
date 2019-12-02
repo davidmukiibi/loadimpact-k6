a4b1cdb1fb7218274c3d852371bdbd02e84b6cf7
David Mukiibi <david.mukiibiq@gmail.com>
2019-12-02 21:33:55 +0300

    changing security group ID

--- permit.sh
+++ permit.sh
@@ -4,4 +4,4 @@ set -ex
 
 public_ip_address=$(curl -q http://checkip.amazonaws.com)
 
-/root/bin/aws ec2 authorize-security-group-ingress --group-id sg-811dfec4 --ip-permissions "[{\"IpProtocol\": \"tcp\", \"FromPort\": 443, \"ToPort\": 443, \"IpRanges\": [{\"CidrIp\": \"${public_ip_address}/32\"}]}]"
+/root/bin/aws ec2 authorize-security-group-ingress --group-id sg-017486265c710d51a --ip-permissions "[{\"IpProtocol\": \"tcp\", \"FromPort\": 443, \"ToPort\": 443, \"IpRanges\": [{\"CidrIp\": \"${public_ip_address}/32\"}]}]"
