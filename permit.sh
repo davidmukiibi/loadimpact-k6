da59c169720a4670af3794630d0b47a4f3509166
David Mukiibi <david.mukiibiq@gmail.com>
2019-12-03 05:27:45 +0300

    changed back the security group

--- permit.sh
+++ permit.sh
@@ -4,4 +4,4 @@ set -ex
 
 public_ip_address=$(curl -q http://checkip.amazonaws.com)
 
-/root/bin/aws ec2 authorize-security-group-ingress --group-name loadimpact-test-sg --ip-permissions "[{\"IpProtocol\": \"tcp\", \"FromPort\": 443, \"ToPort\": 443, \"IpRanges\": [{\"CidrIp\": \"${public_ip_address}/32\"}]}]"
+/root/bin/aws ec2 authorize-security-group-ingress --group-id sg-006b570caa40214f1 --ip-permissions "[{\"IpProtocol\": \"tcp\", \"FromPort\": 443, \"ToPort\": 443, \"IpRanges\": [{\"CidrIp\": \"${public_ip_address}/32\"}]}]"
