c926b8fd104464a9fc5dc76c4cc1260f45b97fba
David Mukiibi <david.mukiibiq@gmail.com>
2019-12-03 06:20:13 +0300

    final edit

--- permit.sh
+++ permit.sh
@@ -4,14 +4,4 @@ set -ex
 
 public_ip_address=$(curl -q http://checkip.amazonaws.com)
 
-# allow() {
-    /root/bin/aws ec2 authorize-security-group-ingress --group-id sg-006b570caa40214f1 --ip-permissions "[{\"IpProtocol\": \"tcp\", \"FromPort\": 443, \"ToPort\": 443, \"IpRanges\": [{\"CidrIp\": \"${public_ip_address}/32\"}]}]"
-#     exit 0
-# }
-
-# main() {
-#     allow
-#     exit 1
-# }
-
-# main "$@"
+/root/bin/aws ec2 authorize-security-group-ingress --group-id sg-006b570caa40214f1 --ip-permissions "[{\"IpProtocol\": \"tcp\", \"FromPort\": 443, \"ToPort\": 443, \"IpRanges\": [{\"CidrIp\": \"${public_ip_address}/32\"}]}]"
--- revoke.sh
+++ revoke.sh
@@ -4,14 +4,4 @@ set -ex
 
 public_ip_address=$(curl -q http://checkip.amazonaws.com)
 
-# deny() {
-    /root/bin/aws ec2 revoke-security-group-ingress --group-id sg-006b570caa40214f1 --ip-permissions "[{\"IpProtocol\": \"tcp\", \"FromPort\": 443, \"ToPort\": 443, \"IpRanges\": [{\"CidrIp\": \"${public_ip_address}/32\"}]}]"
-#     exit 0
-# }
-
-# main() {
-#     deny
-#     exit 1
-# }
-
-# main "$@"
+/root/bin/aws ec2 revoke-security-group-ingress --group-id sg-006b570caa40214f1 --ip-permissions "[{\"IpProtocol\": \"tcp\", \"FromPort\": 443, \"ToPort\": 443, \"IpRanges\": [{\"CidrIp\": \"${public_ip_address}/32\"}]}]"
