07830b0541949a8269b06e089179d3a2a31ca94f
David Mukiibi <david.mukiibiq@gmail.com>
2019-12-03 05:48:03 +0300

    fixing a few typos

--- permit.sh
+++ permit.sh
@@ -4,4 +4,12 @@ set -ex
 
 public_ip_address=$(curl -q http://checkip.amazonaws.com)
 
-/root/bin/aws ec2 authorize-security-group-ingress --group-id sg-006b570caa40214f1 --ip-permissions "[{\"IpProtocol\": \"tcp\", \"FromPort\": 443, \"ToPort\": 443, \"IpRanges\": [{\"CidrIp\": \"${public_ip_address}/32\"}]}]"
+allow() {
+    /root/bin/aws ec2 authorize-security-group-ingress --group-id sg-006b570caa40214f1 --ip-permissions "[{\"IpProtocol\": \"tcp\", \"FromPort\": 443, \"ToPort\": 443, \"IpRanges\": [{\"CidrIp\": \"${public_ip_address}/32\"}]}]"
+}
+
+main() {
+    allow
+}
+
+main "$@"
--- revoke.sh
+++ revoke.sh
@@ -4,5 +4,12 @@ set -ex
 
 public_ip_address=$(curl -q http://checkip.amazonaws.com)
 
-/root/bin/aws ec2 revoke-security-group-ingress --group-id sg-006b570caa40214f1 --ip-permissions IpProtocol=tcp,FromPort=443,ToPort=443,IpRanges='[{CidrIp=${public_ip_address}/32}]'
+deny() {
+    /root/bin/aws ec2 revoke-security-group-ingress --group-id sg-006b570caa40214f1 --ip-permissions IpProtocol=tcp,FromPort=443,ToPort=443,IpRanges='[{CidrIp=${public_ip_address}/32}]'
+}
 
+main() {
+    deny
+}
+
+main "$@"
