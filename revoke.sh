6f5433c601a311244ea1236e1962781dbf950976
David Mukiibi <david.mukiibiq@gmail.com>
2019-12-03 06:01:55 +0300

    made final changes

--- revoke.sh
+++ revoke.sh
@@ -5,7 +5,7 @@ set -ex
 public_ip_address=$(curl -q http://checkip.amazonaws.com)
 
 deny() {
-    /root/bin/aws ec2 revoke-security-group-ingress --group-id sg-006b570caa40214f1 --ip-permissions IpProtocol=tcp,FromPort=443,ToPort=443,IpRanges='[{CidrIp=${public_ip_address}/32}]'
+    /root/bin/aws ec2 revoke-security-group-ingress --group-id sg-006b570caa40214f1 --ip-permissions "[{\"IpProtocol\": \"tcp\", \"FromPort\": 443, \"ToPort\": 443, \"IpRanges\": [{\"CidrIp\": \"${public_ip_address}/32\"}]}]"
     exit 0
 }
 
